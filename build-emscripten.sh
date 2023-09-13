c206309358ea6af6154af639334cb3fef8d654b7
James Chen <dumganhar@gmail.com>
2023-09-13 16:56:25 +0800

    Build static library

--- build-emscripten.sh
+++ build-emscripten.sh
@@ -23,16 +23,51 @@ echo "CPU core count：$core_count"
 current_dir=$(pwd)
 echo "current dir: ${current_dir}"
 
-mkdir -p build
-cd build
-emcmake cmake .. -DCMAKE_BUILD_TYPE=Release
-emmake make twgsl -j${core_count} #VERBOSE=1 
+function build() {
+    BUILD_TYPE=$1
+    BUILD_OUT="build-${BUILD_TYPE}"
+    echo -e "\033[01;32m ------------- BUILD (${BUILD_TYPE}) -----------------  \033[0m"
 
-cd ..
+    CMAKE_BUILD_TYPE="Release"
+    if [[ "${BUILD_TYPE}" == "debug" ]]; then
+        CMAKE_BUILD_TYPE="Debug"
+    fi
+
+    rm -rf ${BUILD_OUT}
+    mkdir -p ${BUILD_OUT}
+    cd ${BUILD_OUT}
+    emcmake cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ..
+    emmake make twgsl -j${core_count} #VERBOSE=1 
+    cd ..
+
+    echo -e "\033[01;32m ------------- COLLECTING ALL .a FILES (${BUILD_TYPE}) -----------------  \033[0m"
+    
+    mkdir -p artifact/${BUILD_TYPE}/tmp
+
+    find ./${BUILD_OUT} -type f -name "*.a" -print0 | xargs -0 -I {} cp {} ./artifact/${BUILD_TYPE}/tmp
+    cp ./Core/twgsl/*.js ./artifact/${BUILD_TYPE}/
+    cp ./Core/twgsl/Source/*.js ./artifact/${BUILD_TYPE}/
+
+    ls -l ./artifact/${BUILD_TYPE}/tmp
+
+    echo -e "\033[01;32m ------------- BUILD A FAT .a (${BUILD_TYPE}) -----------------  \033[0m"
+
+    pushd ./artifact/${BUILD_TYPE}/tmp
+
+    find . -type f -name "*.a" -print0 | xargs -0 -I {} emar -x {}
+
+    emar -rcs libtwgsl-fat.${BUILD_TYPE}.a *.o
+    cp libtwgsl-fat.${BUILD_TYPE}.a ..
+    cd ..
+    rm -rf ./tmp
+
+    popd
+}
+
 rm -rf artifact
-mkdir artifact
-cp ./build/Core/twgsl/twgsl.js ./artifact/twgsl.js
-cp ./build/Core/twgsl/twgsl.wasm ./artifact/twgsl.wasm
+# build "debug"
+build "release"
+
 
 end_time=$(get_current_time_in_seconds)
 
